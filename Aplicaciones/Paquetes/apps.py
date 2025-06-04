74eaa226948442e854de644e5f641b768d7bdace
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-06-04 11:54:51 -0500

    Se agrego Aplicaciones a la aplicacion Paquetes

--- Aplicaciones/Paquetes/apps.py
+++ Aplicaciones/Paquetes/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PaquetesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Paquetes'
+    name = 'Aplicaciones.Paquetes'
