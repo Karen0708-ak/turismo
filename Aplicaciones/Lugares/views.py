b086f89e39912e1ff9d4c1e25977ddd839d049e0
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-06-05 01:56:10 -0500

    Se agrego opciones de agregar lugares

--- Aplicaciones/Lugares/views.py
+++ Aplicaciones/Lugares/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from .models import Lugares
 from django.contrib import messages
@@ -8,3 +8,54 @@ from django.contrib import messages
 def inicio(request):
     listadoLugares=Lugares.objects.all()
     return render(request,"iniciolu.html",{'lugar':listadoLugares})
+def nuevoLugar(request):
+    return render(request,"nuevosLugares.html")
+def guardarLugar(request):
+    nombre=request.POST["nombre"]
+    direccion=request.POST["direccion"]
+    resena=request.POST["resena"]
+    horario=request.FILES.get("horario")
+    historia=request.FILES.get("historia")
+    
+
+    nuevoLugar=Lugares.objects.create(nombre=nombre,direccion=direccion,resena=resena,horario=horario,historia=historia)
+    #mensaje de confirmacion
+    messages.success(request,"Lugar guardada exitosamente")
+    return redirect('iniciolu')
+def eliminarLugar(request,id):
+    lugarEliminar=Lugares.objects.get(id=id)
+    lugarEliminar.delete()
+    #mensaje de confirmacion
+    messages.success(request,"Lugar ELIMINADO exitosamente")
+    return redirect('iniciolu')
+
+def editarLugar(request,id):
+    lugarEditar=Lugares.objects.get(id=id)
+    return render(request,"editarLugares.html",{'lugarEditar':lugarEditar})
+
+def procesarEdicionCiudad(request):
+    nombre=request.POST["nombre"]
+    direccion=request.POST["direccion"]
+    resena=request.POST["resena"]
+    horario=request.FILES.get("horario")
+    historia=request.FILES.get("historia")
+    #Subiendo archivo
+    nhorario=request.FILES.get("horario")
+    #Archivo
+    nhistoria=request.FILES.get("historia")
+
+
+    lugar=Lugares.objects.get(id=id)
+    lugar.nombre=nombre
+    lugar.direccion=direccion
+    lugar.resena=resena
+
+    if nhorario:
+        lugar.horario= nhorario
+    if nhistoria:
+        lugar.historia= nhistoria
+    lugar.save()
+
+    #mensaje de confirmacion
+    messages.success(request,"Lugar ACTUALIZADo exitosamente")
+    return redirect('iniciolu')
