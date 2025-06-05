3b0e19fa1167f9329778f25076dced7a5fb814cf
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-06-04 20:44:17 -0500

    Se agrego la imagen a la plantilla

--- Aplicaciones/Ciudad/views.py
+++ Aplicaciones/Ciudad/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Ciudad
 # Create your views here.
+
 def inicio(request):
     listadoCiudad=Ciudad.objects.all()
     return render(request,"inicioci.html",{'ciudad':listadoCiudad})
