61f0e6e076d7fe2c15a00089ef4d9057e95d9e92
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-06-05 01:26:50 -0500

    Se agrego la vista de inicio a la aplicacion Lugares

--- Aplicaciones/Lugares/views.py
+++ Aplicaciones/Lugares/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Lugares
+from django.contrib import messages
+
 # Create your views here.
+
+def inicio(request):
+    listadoLugares=Lugares.objects.all()
+    return render(request,"iniciolu.html",{'lugar':listadoLugares})
