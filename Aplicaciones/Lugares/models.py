ee7b7b6ba085ef6762ee064e654e35dd900bcbde
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-06-05 00:12:16 -0500

    Se agregaron las migraciones para la aplicacion Lugares

--- Aplicaciones/Lugares/models.py
+++ Aplicaciones/Lugares/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Lugares(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=100)
+    resena = models.TextField()
+    horario = models.FileField(upload_to='lugares')
+    historia = models.FileField(upload_to='lugares')
+
