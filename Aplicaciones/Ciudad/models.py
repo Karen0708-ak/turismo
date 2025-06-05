44c1b4d6ab3b4a1acc7168c7182fe28261a477e4
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-06-05 00:38:25 -0500

    Se agregaron parte las modificaciones para el campos lugares en ciudad

--- Aplicaciones/Ciudad/models.py
+++ Aplicaciones/Ciudad/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from Aplicaciones.Lugares.models import Lugares
 # Create your models here.
 class Ciudad(models.Model):
     CLIMA_CHOICES = [
@@ -9,5 +10,6 @@ class Ciudad(models.Model):
     nombre = models.CharField(max_length=100)
     clima = models.CharField(max_length=10, choices=CLIMA_CHOICES)
     poblacion = models.IntegerField()
+    lugares = models.ManyToManyField(Lugares,null=True, blank=True)
     foto = models.FileField(upload_to ='docu',null=True, blank=True)
     folleto = models.FileField(upload_to='docu',null=True, blank=True)
--- Aplicaciones/Ciudad/views.py
+++ Aplicaciones/Ciudad/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import Ciudad
+from .models import Lugares
 # Create your views here.
 from django.contrib import messages
 
@@ -13,13 +14,17 @@ def guardarCiudad(request):
     nombre=request.POST["nombre"]
     clima=request.POST["clima"]
     poblacion=request.POST["poblacion"]
-
+    lugares_ids = request.POST.getlist("lugares")
     #Subiendo archivo
     foto=request.FILES.get("foto")
     #Archivo
     folleto=request.FILES.get("folleto")
+    
 
     nuevaCiudad=Ciudad.objects.create(nombre=nombre,clima=clima,poblacion=poblacion,foto=foto,folleto=folleto)
+    for lugar_id in lugares_ids:
+            lugares = Lugares.objects.get(id=lugar_id)
+            nuevaCiudad.lugares.add(lugares)
     #mensaje de confirmacion
     messages.success(request,"Ciudad guardada exitosamente")
     return redirect('inicioci')
@@ -32,7 +37,13 @@ def eliminarCiudad(request,id):
 
 def editarCiudad(request,id):
     ciudadEditar=Ciudad.objects.get(id=id)
-    return render(request,"editarCiudad.html",{'ciudadEditar':ciudadEditar})
+    rlugares=Lugares.objects.all()
+    lugares_actuales = ciudadEditar.lugares.values_list('id', flat=True)
+    return render(request, "editarCiudad.html", {
+        'ciudadEditar':ciudadEditar,
+        'lugares': rlugares,
+        'lugares_actuales': lugares_actuales
+    })
 
 def procesarEdicionCiudad(request):
     id=request.POST["id"]
@@ -40,6 +51,7 @@ def procesarEdicionCiudad(request):
     clima=request.POST["clima"]
     poblacion=request.POST["poblacion"]
 
+    lugares_ids = request.POST.getlist("lugares")
     #Subiendo archivo
     nfoto=request.FILES.get("foto")
     #Archivo
@@ -56,6 +68,11 @@ def procesarEdicionCiudad(request):
     if nfolleto:
         ciudad.folleto= nfolleto
     ciudad.save()
+    ciudad.lugares.clear()
+    for id in lugares_ids:
+            lugares = Lugares.objects.get(id_jugador=id)
+            ciudad.lugares.add(lugares)
+
     #mensaje de confirmacion
     messages.success(request,"Ciudad ACTUALIZADA exitosamente")
     return redirect('inicioci')
